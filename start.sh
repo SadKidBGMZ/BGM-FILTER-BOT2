dd4ac8a220ad320d86eb61805b2390754adf6bce
SK Rajii࿐ <104976581+SadKidBGMZ@users.noreply.github.com>
2022-09-01 12:32:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/SadKidBGMZ/KR-AUTO-FILTER-BOT.git /KR-AUTO-FILTER-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /KR-AUTO-FILTER-BOT
 fi
-cd /EvaMaria
+cd /KR-AUTO-FILTER-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
